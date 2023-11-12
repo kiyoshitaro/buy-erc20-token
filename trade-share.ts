5653ff3b11e58beeb5c4ed413ca81adc91fe845e
kiyoshitaro <hunglhp1998@gmail.com>
2023-11-13 00:42:01 +0700

    update trade share

--- trade-share.ts
+++ trade-share.ts
@@ -1,10 +1,11 @@
-import { BigNumber, Contract,  Wallet, ethers } from "ethers";
+import { BigNumber, Contract,  PopulatedTransaction,  VoidSigner,  Wallet, ethers } from "ethers";
 import abi from './abis/starleague.json';
 import * as dotenv from "dotenv";
 import { formatEther } from "ethers/lib/utils";
+import { TransactionRequest } from "zksync-web3/build/src/types";
 dotenv.config({ path: '.env' });
 
-const BOOST_MIN_PRICE = 1.25;
+const BOOST_MIN_PRICE = 1.2;
 const STRONG_BOTS = [
   '0xf4ef66a43bdf743cf22c0da76d8510f04bfcf79c', //luckydjj88 
   '0x5fb2ee869c31e94b098aaaf2351cd37a56d14d42', //unknown
@@ -37,12 +38,13 @@ const sellShare = async(subjectAddress: string, retry: number = 0) =>{
       1,
       {
         // gasLimit: BigNumber.from(500000),
+        gasPrice: BigNumber.from(2660000000000),
         // maxPriorityFeePerGas: BigNumber.from(2510000000000), // 2500 gwei        
         // maxPriorityFeePerGas: 2510, //1.5 -2 gwei
       }
     );
     const trx = await sellShares.wait();
-    console.log("🚀 ~ file: trade-share.ts:45 ~ sellShare ~ sellShares:", trx.transactionHash,Number(trx.gasUsed.mul(BigNumber.from(2500)).toString())/ (1000000000) );
+    console.log("🚀 ~ file: trade-share.ts:45 ~ sellShare ~ sellShares:", trx.transactionHash);
   } catch (error) {
     console.log("🚀 ~ file: trade-share.ts:36 ~ sellShare ~ error:", error)
 
@@ -67,7 +69,7 @@ const sellShare = async(subjectAddress: string, retry: number = 0) =>{
 const autoBidShare = async(subjectAddress: string, price:number, times: number = 1) =>{
   const currentTime = new Date().getTime();
   const endBiddingTime = await getBiddingTime(subjectAddress);
-  setTimeout(() => bidShare(subjectAddress, price, times), endBiddingTime - currentTime - 18000);
+  setTimeout(() => bidShare(subjectAddress, price, times), endBiddingTime - currentTime - 21000);
 }
 const bidShare = async(subjectAddress: string, price:number, times: number = 1) =>{
   try {
@@ -79,13 +81,35 @@ const bidShare = async(subjectAddress: string, price:number, times: number = 1) 
     for(let i =0; i< times; i++){
       contract.connect(wallet).buyShares(
         subjectAddress,
-        {value: _price, nonce: transactionCount + i}
+        {
+          value: _price,
+          gasPrice: BigNumber.from(2660000000000),
+          nonce: transactionCount + i
+        }
       );
     }
   } catch (error) {
     console.log("🚀 ~ file: trade-share.ts:64 ~ bidShare ~ error:", error)
   }
 }
+
+const buyShare = async(subjectAddress: string) =>{
+  try {
+    const _t = await getBuyPriceAfterFee(subjectAddress);   
+    console.log("🚀 ~ Buy at :", _t)
+    const _price = ethers.utils.parseEther(_t.toString()).toHexString();
+      contract.connect(wallet).buyShares(
+        subjectAddress,
+        {
+          value: _price,
+          // gasPrice: BigNumber.from(2660000000000),
+        }
+      );
+  } catch (error) {
+    console.log("🚀 ~ file: trade-share.ts:64 ~ bidShare ~ error:", error)
+  }
+}
+
 
 const getRecommendBidPrice = async (subjectAddress: string, defaultPrice = 1) => {
   try {
@@ -123,6 +147,16 @@ const getSellPriceAfterFee = async (subjectAddress: string) => {
   console.log("🚀 ~ file: trade-share.ts:92 ~ getSellPriceAfterFee ~ result:", formatEther(result).toString())
 }
 
+
+const getBuyPriceAfterFee = async (subjectAddress: string) => {
+  const result = await contract.getBuyPriceAfterFee(
+    subjectAddress,
+    1,
+  );
+  console.log("🚀 ~ file: trade-share.ts:157 ~ getBuyPriceAfterFee ~ ethers.utils.parseEther(result.toString()).toHexString():",Number(formatEther(result)), ethers.utils.parseEther(result.toString()).toHexString())
+  return Number(formatEther(result))
+}
+
 const getListBidPrice = async (subjectAddress: string, defaultPrice = 1) => {
   const result = await contract.getPoolInitialTops(
     subjectAddress,
@@ -130,22 +164,103 @@ const getListBidPrice = async (subjectAddress: string, defaultPrice = 1) => {
   return result.map(res => `${res.account} - ${Number(formatEther(res.amount).toString())}`);
 }
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+let updatedNonceAt = Date.now();
+let nonce = 0;
+const updateNonce = async () => {
+  while (true) {
+    console.log("run");
+    try {
+      if (
+        updatedNonceAt + 1000 * 7 < Date.now()
+      ) {
+        nonce = await wallet.getTransactionCount();
+        console.log("🚀 ~ file: trade-share.ts:148 ~ updateNonce ~ nonce:", nonce)
+        updatedNonceAt = Date.now();
+        await sleep(5000);
+      }
+    } catch (e) {
+      console.log('Error get nonce2: ', e);
+    }
+  }
+}
+
 (async () => {
-  const subAddress = '0x96b797cd56d2d5a161b7e27f48af38cc63ed7a89';
+  const subAddress = '0x71f0db8a29cdb04a69400dc061585070e673d4b2';
 
-  // console.log("🚀 ~ file: trade-share.ts:136 ~ await getBiddingTime(subAddress):", new Date(await getBiddingTime(subAddress)))
-  // console.log("============ List bids ============", await getListBidPrice(subAddress));
-  // await getSellPriceAfterFee(subAddress);
-  // console.log(await getRecommendBidPrice(subAddress));
+  console.log("🚀 ~ file: trade-share.ts:136 ~ await getBiddingTime(subAddress):", new Date(await getBiddingTime(subAddress)))
+  console.log("============ List bids ============", await getListBidPrice(subAddress));
+  await getSellPriceAfterFee(subAddress);
+  // await getBuyPriceAfterFee(subAddress);
+  console.log(await getRecommendBidPrice(subAddress));
 
-  // await bidShare(subAddress, 1, 1);  
+  // await buyShare(subAddress);  
+  // await bidShare(subAddress ,1 ,1);  
   // await sellShare(subAddress, 1);
 
-  // await autoBidShare(subAddress, 1, 2);  
+  // await autoBidShare(subAddress, 1, 1);  
   // await autoSellShare(subAddress, 1);
+
+  // const transactionHash = '0x7fe152653415da8bd2c6d24e932da2bcfead4e48a9697dfa804a06b026a3ca0c';
+  // const [transactions, transactionOrbiter] = await Promise.all([
+  //   chiliz_provider.getTransactionReceipt(transactionHash),
+  //   chiliz_provider.getTransaction(transactionHash),
+  // ]);
+  // const _t = await transactionOrbiter.wait()
+  // console.log("🚀 ~ file: trade-share.ts:155 ~ _t:", _t)
+  // console.log("🚀 ~ file: trade-share.ts:154 ~ transactions:", ethers.utils.formatEther(transactionOrbiter.gasPrice.mul(transactionOrbiter.gasLimit)).toString() )
+
+
+  // const fromBlock = 7945000
+  // const latestBlock = Math.min(
+  //   await chiliz_provider.getBlockNumber(),
+  //   fromBlock + 10,
+  // );
+  // const eventBuyFilter = contract.filters.Trade();
+  // const buyLogs = await contract.queryFilter(
+  //   eventBuyFilter,
+  //   fromBlock,
+  //   Number(latestBlock),
+  // );
+  // console.log("🚀 ~ file: trade-share.ts:170 ~ buyLogs:", buyLogs)
+
+
+  // const _price = ethers.utils.parseEther('1').toHexString();
+  // const response = await contract.populateTransaction.buyShares(
+  //   subAddress,
+  //   {value: _price}
+  // );
+  // response.gasLimit = BigNumber.from(40000000000000);
+  // response.gasPrice = BigNumber.from(2650000000000);
+  // console.log("🚀 ~ file: trade-share.ts:152 ~ response:", response)
+  // const voidSigner = new VoidSigner(wallet.address, chiliz_provider);
+  // const _data = await voidSigner.populateTransaction(response);
+  // console.log("🚀 ~ file: trade-share.ts:156 ~ _data:", _data)
+  // const data = toStringTransaction(_data);
+  // console.log("🚀 ~ file: trade-share.ts:160 ~ data:", data)
 
   // const block = await chiliz_provider.getBlock(1)
   // console.log("🚀 ~ file: trade-share.ts:96 ~ block:", new Date(block.timestamp*1000))
+
+
+  // updateNonce().then().catch();
+  // console.log("hrhrhr");
 })()
 
-// 0x7af4c68b7d89a53f4dd47c9aea26dd90f673c03e
+
+const toStringTransaction = (
+  populatedTransaction: PopulatedTransaction | TransactionRequest,
+) => {
+  const transaction = { ...populatedTransaction };
+  Object.keys(transaction).forEach((key) => {
+    if (typeof transaction[key] === 'object') {
+      transaction[key] = populatedTransaction[key].toString();
+    }
+  });
+  return transaction;
+};
+
+
+// 0x65a9f921dc6184cbadce4f66cdbd6b05fafdd778
